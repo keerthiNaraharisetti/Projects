41a80e60f384d0c86c566d91b01a77762550f654
keerthiNaraharisetti <142656654+keerthiNaraharisetti@users.noreply.github.com>
2024-01-26 12:01:52 +0530

    Add files via upload

---
+++ Basic Spring boot/SpringBoot-graddle/basic/src/main/java/com/App/basic/BasicApplication.java
@@ -0,0 +1,14 @@
+package com.App.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplication.class, args);
+		System.out.println("Hi Keerthi !");
+	}
+
+}
